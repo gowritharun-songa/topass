e7b0af3d5e588e6f5a381550f3d9795eeab72612
Tharun <gowritharun461@gmail.com>
2026-08-02 19:24:31 +0530

    Changed all controllers to helper handler

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -25,7 +25,7 @@ export const getUsers = asyncHandler(async (req, res) => {
     });
 });
 
-export const getUserById = asyncHandler( async (req, res, next) => {
+export const getUserById = asyncHandler( async (req, res) => {
     const { id } = req.params;
     const user = await User.findById(id);
 
@@ -38,85 +38,68 @@ export const getUserById = asyncHandler( async (req, res, next) => {
     });
 });
 
-export const postUser = async (req, res, next) => {
-    try {
-        const { name, age, email, password, role } = req.body;
-        if(!name || !email || !password) {
-            throw new AppError("All Fields are required", 400);
-        }
-
-        let user = await User.findOne({email});
-        if(user) {
-            throw new AppError("User already Exists", 400);
-        }
-
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        const newUser = await User.create({
-            name, age, email, password: hashedPassword, role: "user"
-        });
-
-        return res.status(201).json({
-            message: "User created",
-            data: newUser
-        });
-
-    } catch(error) {
-        next(error);
+export const postUser = asyncHandler( async (req, res) => {
+    const { name, age, email, password, role } = req.body;
+    if(!name || !email || !password) {
+        throw new AppError("All Fields are required", 400);
     }
-}
 
-export const putUser = async (req, res, next) => {
+    let user = await User.findOne({email});
+    if(user) {
+        throw new AppError("User already Exists", 400);
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await User.create({
+        name, age, email, password: hashedPassword, role: "user"
+    });
+
+    return res.status(201).json({
+        message: "User created",
+        data: newUser
+    });
+});
+
+export const putUser = asyncHandler( async (req, res, next) => {
     const { id } = req.params;
     const { name, age, email, password } = req.body;
 
-    try {
-        let updatedData = { name, age, email, password };
-        const updatedUser = await User.findByIdAndUpdate(id, updatedData, {new: true, runValidators: true});
-        if(!updatedUser) {
-            throw new AppError("User not Found", 404);
-        }
-
-        return res.status(200).json({
-            message: "User updated Successfully",
-            data: updatedUser
-        })
-
-    } catch(error) {
-        next(error);
+    let updatedData = { name, age, email, password };
+    const updatedUser = await User.findByIdAndUpdate(id, updatedData, {new: true, runValidators: true});
+    if(!updatedUser) {
+        throw new AppError("User not Found", 404);
     }
-}
 
-export const patchUser = async (req, res, next) => {
+    return res.status(200).json({
+        message: "User updated Successfully",
+        data: updatedUser
+    })
+});
+
+// Update user by the fields only gotten provided
+export const patchUser = asyncHandler( async (req, res, next) => {
     const { id } = req.params;
-    try {
-        const updatedUser = await User.findByIdAndUpdate(id, req.body, { new: true, runValidators: true });
-        if(!updatedUser) {
-            throw new AppError("User not Found", 404);
-        }
-        return res.status(200).json({
-            message: "User updated Successfully",
-            data: updatedUser
-        });
-    } catch(error) {
-        next(error);
+    const updatedUser = await User.findByIdAndUpdate(id, req.body, { new: true, runValidators: true });
+    if(!updatedUser) {
+        throw new AppError("User not Found", 404);
     }
-}
+    return res.status(200).json({
+        message: "User updated Successfully",
+        data: updatedUser
+    });
+});
 
-export const deleteUser = async (req, res, next) => {
+// Delete user
+export const deleteUser = asyncHandler( async (req, res, next) => {
     const {id} = req.params;
-    try {
-        const deletedUser = await User.findByIdAndDelete(id);
-        if(!deletedUser) {
-            throw new AppError("User not Found", 404);
-        }
-        return res.status(200).json({
-            message: "User Deleted Successfully",
-            data: deletedUser
-        })
-
-    } catch(error) {
-        next(error);
+    const deletedUser = await User.findByIdAndDelete(id);
+    if(!deletedUser) {
+        throw new AppError("User not Found", 404);
     }
-}
+    return res.status(200).json({
+        message: "User Deleted Successfully",
+        data: deletedUser
+    })
+});
